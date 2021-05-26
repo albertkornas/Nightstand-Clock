12d1d2b028ba12ffc1847a1a501b81ec562cebf7
albertkornas <auk7@psu.edu>
2021-05-26 12:08:23 -0400

    Animations - Each digit that changes receives an animation (currently, opacity)

--- TestClock/ContentView.swift
+++ TestClock/ContentView.swift
@@ -10,81 +10,36 @@ import SwiftUI
 struct ContentView: View {
     @State var date = Date()
     
-    var hourFormat: DateFormatter {
+    var timeFormat: DateFormatter {
         let formatter = DateFormatter()
-        formatter.dateFormat = "h"
+        formatter.dateFormat = "hh:mm:ssa"
         return formatter
     }
     
-    var minuteFormat: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "mm"
-        return formatter
-    }
-    
-    var secondsFormat: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "ss a"
-        return formatter
-    }
-    
-    //Return array of hour, format, and seconds TODO: convert into dict
-    func timeCharacters(date: Date) -> [String] {
-        let hour: String = hourFormat.string(from: date)
-        let minute: String = minuteFormat.string(from: date)
-        let second: String = secondsFormat.string(from: date)
-        let timeArray: [String] = [hour, minute, second]
+    func timeCharactersArray(date: Date) -> [String] {
+        let time: String = timeFormat.string(from: date)
+        let timeArray = time.map{String($0)}
         return timeArray
     }
     
     var body: some View {
         HStack {
-            Group {
-            Text(timeCharacters(date: date)[0])
-                .padding()
-                .background(SwiftUI.Color.gray)
-                .transition(.opacity)
-                .id("digitz" + timeCharacters(date: date)[0].description)
-                .onAppear(perform: {let _ = self.updateSecond})
-                Text(timeCharacters(date: date)[2])
+            ForEach(timeCharactersArray(date: date).indices, id: \.self) { index in
+                withAnimation(.linear(duration: 0.25), {
+                Text(timeCharactersArray(date: date)[index])
                     .padding()
+                    .frame(width: 50, height: 100)
                     .background(SwiftUI.Color.gray)
                     .transition(.opacity)
-                    .id("digitz" + timeCharacters(date: date).description)
-                    .onAppear(perform: {let _ = self.updateSecond})
+                    .id(timeCharactersArray(date: date)[index].description)
+                    .onAppear(perform: {let _ = self.updateTimer})
+                })
             }
+            
         }
     }
     
-   /* var updateTimer: Timer {
-         Timer.scheduledTimer(withTimeInterval: 1, repeats: true,
-                              block: {_ in
-                                 self.date = Date()
-                                
-                               })
-            
-    }
-    */
-    
-    var updateHour: Timer {
-         Timer.scheduledTimer(withTimeInterval: 1, repeats: true,
-                              block: {_ in
-                                 self.date = Date()
-                                
-                               })
-            
-    }
-    
-    var updateMinute: Timer {
-         Timer.scheduledTimer(withTimeInterval: 1, repeats: true,
-                              block: {_ in
-                                 self.date = Date()
-                                
-                               })
-            
-    }
-    
-    var updateSecond: Timer {
+    var updateTimer: Timer {
          Timer.scheduledTimer(withTimeInterval: 1, repeats: true,
                               block: {_ in
                                 withAnimation(.linear(duration: 0.25), {
@@ -94,10 +49,4 @@ struct ContentView: View {
             
     }
     
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
 }
